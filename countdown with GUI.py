44926fd9cf5d6fc84cc110edc113ab5e54878850
Laksaya Mahalingam <63893444+sm-laksh5@users.noreply.github.com>
2020-10-06 07:09:49 +0530

    Add files via upload
    Graphical User Interface Countdown Timer

---
+++ countdown with GUI.py
@@ -0,0 +1,81 @@
+import time
+from tkinter import *
+from tkinter import messagebox
+ 
+ 
+# creating Tk window
+root = Tk()
+  
+# setting geometry of tk window
+root.geometry("300x250")
+  
+# Using title() to display a message in
+# the dialogue box of the message in the
+# title bar.
+root.title("Time Counter")
+  
+# Declaration of variables
+hour=StringVar()
+minute=StringVar()
+second=StringVar()
+  
+# setting the default value as 0
+hour.set("00")
+minute.set("00")
+second.set("00")
+  
+# Use of Entry class to take input from the user
+hourEntry= Entry(root, width=3, font=("Arial",18,""),
+                 textvariable=hour)
+hourEntry.place(x=80,y=20)
+  
+minuteEntry= Entry(root, width=3, font=("Arial",18,""),
+                   textvariable=minute)
+minuteEntry.place(x=130,y=20)
+  
+secondEntry= Entry(root, width=3, font=("Arial",18,""),
+                   textvariable=second)
+secondEntry.place(x=180,y=20)
+  
+  
+def submit():
+    try:
+        
+        temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
+    except:
+        print("Please input the right value")
+    while temp >-1:
+         
+        
+        mins,secs = divmod(temp,60) 
+  
+        
+        hours=0
+        if mins >60:
+             
+            
+            hours, mins = divmod(mins, 60)
+         
+        
+        hour.set("{0:2d}".format(hours))
+        minute.set("{0:2d}".format(mins))
+        second.set("{0:2d}".format(secs))
+  
+       
+        root.update()
+        time.sleep(1)
+  
+        
+        if (temp == 0):
+            messagebox.showinfo("Time Countdown", "Time's up ")
+         
+       
+        temp -= 1
+ 
+# button widget
+btn = Button(root, text='Set Time Countdown', bd='5',
+             command= submit)
+btn.place(x = 70,y = 120)
+  
+
+root.mainloop()
